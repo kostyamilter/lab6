ff55107549c0c4251515a9467a03b8b2c993e688
milter.kostya <milter.kostya@gmail.com>
2024-12-28 02:16:22 +0300

    sdfsdf

--- api/index.test.js
+++ api/index.test.js
@@ -10,7 +10,7 @@ describe("POST /register", () => {
       password: "testpassword",
     });
 
-    expect(response.body).toHaveProperty("accessToken1");
+    expect(response.body).toHaveProperty("accessToken");
     expect(response.body).toHaveProperty("refreshToken");
   });
 });
